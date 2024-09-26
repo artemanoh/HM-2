5949b0c566eba011516a7666e8724a37d817ff87
artem1214 <61391756+artemanoh@users.noreply.github.com>
2024-09-26 14:19:14 +0300

    make

---
+++ index.js
@@ -0,0 +1,18 @@
+const temperatureInCelsius = 30;
+const temperatureInFahrenheit = (9 / 5) * temperatureInCelsius + 32;
+console.log(temperatureInFahrenheit);
+
+const numberOfDays = 31;
+const numberOfHours = numberOfDays * 24;
+const numberOfMinutes = numberOfHours * 60;
+console.log(numberOfHours);
+console.log(numberOfMinutes);
+
+const healthPlayer = 65;
+const energyPlayer = 55;
+console.log(healthPlayer - 33);
+console.log(energyPlayer - 14);
+
+const purchaseAmount = 1500;
+const discount = 9 / 10;
+console.log(purchaseAmount * discount);
