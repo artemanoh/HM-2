11ec446735491bf87f1a6866a68fd29544084955
artem1214 <61391756+artemanoh@users.noreply.github.com>
2024-11-15 16:21:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,27 @@ console.log(energyPlayer - 14);
 const purchaseAmount = 1500;
 const discount = 9 / 10;
 console.log(purchaseAmount * discount);
+
+const number = 70.2;
+console.log(Math.floor(number));
+
+
+const stringOfFloat = "35.2347";
+console.log(Number.parseFloat(stringOfFloat));
+
+const stringOfInt = "213";
+console.log(Number.parseFloat(stringOfInt));
+
+const rootOfNumber = 225;
+console.log(Math.sqrt(rootOfNumber));
+
+const integer = 856;
+let stringOfNumbers = "50384";
+stringOfNumbers = Number.parseInt(stringOfNumbers);
+
+console.log(integer);
+console.log(stringOfNumbers);
+console.log(integer.toString());
+console.log(stringOfNumbers.toString());
+
+
